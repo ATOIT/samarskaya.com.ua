6455b7758a93afd447a76e27eb4e08c7c9a073ee
SergeyDerko <sergeyderko@gmail.com>
2017-09-24 19:28:35 +0300

    final admin panel + add new style

---
+++ AtoITCore/AtoITCore/Domain/Domain/Entityes/Order.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entityes
+{
+    public class Order : OrderDetails
+    {
+        //public Order()
+        //{
+        //    Products = new List<Product>();
+        //}
+        
+        [Key]
+        [ScaffoldColumn(false)]
+        public int OrderId { get; set; }
+
+        [Display(Name = "Статус заказу")]
+        [Required]
+        public string Status { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+        
+      }
+}
